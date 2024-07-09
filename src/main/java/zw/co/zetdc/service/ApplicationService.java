4c052745b36d77ad6431a14602bc2aae9d1d44e7
kuly87 <69579254+kuly87@users.noreply.github.com>
2024-07-09 10:56:27 +0200

    Add new status, give user update role, add user to approve endpoint

--- src/main/java/zw/co/zetdc/service/ApplicationService.java
+++ src/main/java/zw/co/zetdc/service/ApplicationService.java
@@ -1,5 +1,7 @@
 package zw.co.zetdc.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import zw.co.zetdc.entities.Application;
 import zw.co.zetdc.enums.District;
 import zw.co.zetdc.enums.Region;
@@ -19,6 +21,7 @@ public interface ApplicationService {
 
 
     public List<Application> getApplicationsByDistrict(District district);
+    Page<Application> getAllApplicationsByDistrict(District district, int page , int size);
 
 
 //    public List<Application> getApplicationsByEmail(String email);
@@ -34,5 +37,26 @@ public interface ApplicationService {
     Application updateLineItem(Long applicationId, Long lineItemId, String reason, Status status);
 
 
+    List<Application> getApplicationBySearch(String createdby, Long applicationId);
 
+    List<Application> getDistrictApplicationBySearch(District district, Long applicationId);
+
+    //List<Application> getPendingApplicationsByDistrict(District district);
+    Page<Application> getPendingApplicationsByDistrict(District district, int page , int size);
+
+    List<Application> getPendingDistrictApplicationBySearch(District district, Long applicationId);
+
+    List<Application> getApplicationsByDistrictForInspection(District district);
+
+    List<Application> getApplicationForInspectionBySearch(District district, Long applicationId);
+
+    List<Application> getPendingApplicationsByDistrictForInspection(District district);
+
+    List<Application> getPendingApplicationForInspectionBySearch(District district, Long applicationId);
+
+    List<Application> getApplicationForStoresBySearch(District district, Long applicationId);
+
+    List<Application> getAllApplicationsByDistrictForStores(District district);
+
+    List<Application> getNumApplicationsByDistrict(District district);
 }
