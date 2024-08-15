1b40dcedf5142e75e9ba44bee9d18a35e3d2ae3a
emagodi <magodiedwin@gmail.com>
2024-08-15 10:45:58 +0200

    and id to response

--- src/main/java/zw/co/zetdc/payload/response/ApplicationResponse.java
+++ src/main/java/zw/co/zetdc/payload/response/ApplicationResponse.java
@@ -22,6 +22,7 @@ import java.util.List;
 @NoArgsConstructor
 public class ApplicationResponse {
 
+    private Long id;
     private String title;
     private String firstname;
     private String lastname;
