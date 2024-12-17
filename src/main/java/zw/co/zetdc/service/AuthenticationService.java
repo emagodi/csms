34b8b5f8de8c5f83bd409da4abe4151242b67c4a
emagodi <magodiedwin@gmail.com>
2024-12-17 12:07:24 +0200

    Update authentication service

--- src/main/java/zw/co/zetdc/service/AuthenticationService.java
+++ src/main/java/zw/co/zetdc/service/AuthenticationService.java
@@ -1,10 +1,22 @@
 package zw.co.zetdc.service;
 
+import zw.co.zetdc.entities.User;
 import zw.co.zetdc.payload.request.AuthenticationRequest;
 import zw.co.zetdc.payload.request.RegisterRequest;
+import zw.co.zetdc.payload.request.UserUpdateRequest;
 import zw.co.zetdc.payload.response.AuthenticationResponse;
 
+
 public interface AuthenticationService {
-    AuthenticationResponse register(RegisterRequest request);
+
+    AuthenticationResponse register(RegisterRequest request, boolean createdByAdmin, String token);
     AuthenticationResponse authenticate(AuthenticationRequest request);
+
+    public User getUserById(Long id);
+
+
+    public User updateUser(Long userId, UserUpdateRequest userUpdateRequest);
+
+    public void changePassword(String email, String currentPassword, String newPassword);
+
 }
